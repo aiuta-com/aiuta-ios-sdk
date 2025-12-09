0191787b0e00ca770558a97978633441edf8becb
Sergei Nevesenko <sergei.nevesenko@aiuta.com>
2025-12-09 10:14:22 +0200

    Version UP

--- Sources/AiutaSdk/Solution/Sdk/Sdk.swift
+++ Sources/AiutaSdk/Solution/Sdk/Sdk.swift
@@ -15,7 +15,7 @@
 import Foundation
 
 enum Sdk {
-    static let version = "4.2.3"
+    static let version = "4.2.4"
 
     @available(iOS 13.0.0, *)
     @MainActor static var isForeground: Bool {
